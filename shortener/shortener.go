3e0d61f0c28721f5665c4739b0a2a4587cb1729e
nem0xff <nem0xff@gmail.com>
2019-06-02 16:14:38 +0300

    Added shortener

---
+++ shortener/shortener.go
@@ -0,0 +1,69 @@
+package shortener
+
+import (
+	"math"
+	"strings"
+)
+
+const (
+	alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	base     = len(alphabet)
+)
+
+//Shortener - interface of shortener
+type Shortener interface {
+	Shorten(url string) string
+	Resolve(url string) string
+}
+
+// Shorten - main struct
+type Shorten struct {
+	storage   []string
+	hashToNum map[string]string
+}
+
+// Shorten - get short link of url
+func (s *Shorten) Shorten(url string) string {
+	s.storage = append(s.storage, url)
+	lastPosition := len(s.storage) - 1
+	return base10ToNewBase(lastPosition)
+}
+
+// Resolve - resolv short link of url
+func (s *Shorten) Resolve(url string) string {
+	key := newBaseToBase10(url)
+	if key >= len(s.storage) {
+		return ""
+	}
+	return s.storage[key]
+}
+
+func base10ToNewBase(i int) string {
+	var result []byte
+	remain := i
+
+	for remain > base-1 {
+		//fmt.Printf("remain = %v, base = %v, remainbase = %v\n", remain, base, string(alphabet[remain%base]))
+		result = append(result, alphabet[remain%base])
+		remain = remain / base
+	}
+	result = append(result, alphabet[remain]) // добираем последний остаток
+
+	//Реверсируем строку
+	for i, j := 0, len(result)-1; i < j; i, j = i+1, j-1 {
+		result[i], result[j] = result[j], result[i]
+	}
+
+	return string(result)
+}
+
+func newBaseToBase10(number string) int {
+	var result int
+	lenght := len(number) - 1
+	for i, s := range number {
+		index := strings.Index(alphabet, string(s))
+		result = result + index*int(math.Pow(float64(base), float64(lenght-i)))
+		//fmt.Printf("current symbol = %v, i = %v, s = %v, index = %v, result=%v, %v\n", string(s), i, s, index, result, math.Pow(float64(base), float64(i)))
+	}
+	return result
+}
---
+++ shortener/shortener_test.go
@@ -0,0 +1,107 @@
+package shortener
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"testing"
+	"time"
+)
+
+var mystruct Shorten
+
+var src = rand.NewSource(time.Now().UnixNano())
+
+const (
+	letterBytes   = "abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = int(63 / letterIdxBits)
+)
+
+func BenchmarkAddLinks(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		//for j := 0; j < 100000; j++ { // Добавляем 100 000 URL'ов
+		_ = mystruct.Shorten(makePseudoURL())
+		//fmt.Println(key)
+		//}
+	}
+	//PrintMemUsage()
+
+}
+
+func TestShorten(t *testing.T) {
+
+	myURL := makePseudoURL()
+	shortLinkKey := mystruct.Shorten(myURL)
+	if shortLinkKey == "" {
+		t.Error("При добавлении элемента вернулся пустой ключ")
+	}
+}
+
+func TestResolve(t *testing.T) {
+	myURL := makePseudoURL()
+	shortLinkKey := mystruct.Shorten(myURL)
+	if myURL != mystruct.Resolve(shortLinkKey) {
+		t.Error("Вернулся не правильный результат")
+	}
+	if mystruct.Resolve("4Ydo3") != "" {
+		t.Error("Вернулась не пустая строка при запросе несуществующего элемента")
+	}
+}
+
+func TestBase10ToNewBase(t *testing.T) {
+	if base10ToNewBase(73456487) != "4Ydo3" {
+		t.Error("Неверный результат преобразования в новую систему счисления")
+	}
+}
+
+func TestNewBaseToBase10(t *testing.T) {
+	// 4Ydo3 - 73456487
+	if newBaseToBase10("4Ydo3") != 73456487 {
+		t.Error("Неверный результат преобразования в десятичную систему счисления")
+	}
+}
+
+func makePseudoURL() string {
+	var lenght int
+	for lenght = 0; lenght < 10; {
+		lenght = int(src.Int63() & letterIdxMask) // from 1 to 63
+	}
+	// TODO: добавить валидацию. Очевидно ошибка если первые символы "/"
+	return "http://" + genString(lenght)
+}
+
+func genString(n int) string {
+	b := make([]byte, n)
+
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+	return b / 1024 / 1024
+}
