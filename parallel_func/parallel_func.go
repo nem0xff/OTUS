c8698e12682f4e66f837c07e8ea8f54350eca338
Alexey Provalenov <nem0xff@gmail.com>
2019-07-23 11:05:24 +0300

    parallel_func: draft 2

--- parallel_func/parallel_func.go
+++ parallel_func/parallel_func.go
@@ -5,36 +5,54 @@ import (
 	"sync"
 )
 
-func Executer(functions []func() error) error {
-	var wg sync.WaitGroup
+type Task func() error
 
-	var mu sync.Mutex
-	var errorCount int
+type Executer struct {
+	mu            sync.Mutex
+	errorCount    int
+	maxErrorCount int
+	taskCount     int
+	wg            sync.WaitGroup
+}
 
-	for _, function := range functions {
-		fmt.Println(errorCount)
-		if errorCount > 2 {
-			break
+func (e *Executer) startTask(task Task) {
+	defer e.wg.Done()
+	if e.allowWork() {
+		err := task()
+		fmt.Printf("Return from task \"%v\" \n", err)
+		if err != nil {
+			e.mu.Lock()
+			e.errorCount += 1
+			e.mu.Unlock()
 		}
-		wg.Add(1)
-		go func(funcForExec func() error) {
-			if errorCount > 2 {
-				fmt.Println("Кол-во ошибок: ", errorCount)
-				wg.Done()
-				return
-			}
-			err := funcForExec()
-			if err != nil {
-				mu.Lock()
-				errorCount++
-				mu.Unlock()
-			}
+	}
+}
 
-			wg.Done()
+func (e *Executer) startTasks(tasks []Task, maxErrorCount int) error {
 
-		}(function)
+	e.maxErrorCount = maxErrorCount
+
+	for _, task := range tasks {
+		if e.allowWork() {
+			e.wg.Add(1)
+			go e.startTask(task)
+			e.taskCount++
+		}
 
 	}
-	wg.Wait()
+
+	e.wg.Wait()
+
+	fmt.Println(e.errorCount)
+
 	return nil
+
+}
+
+func (e *Executer) allowWork() bool {
+	e.mu.Lock()
+	result := e.errorCount < e.maxErrorCount
+	e.mu.Unlock()
+
+	return result
 }
--- parallel_func/parallel_func_test.go
+++ parallel_func/parallel_func_test.go
@@ -4,25 +4,28 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestExecuter(t *testing.T) {
+	var executer Executer
 
-	pf := getFuncs(20)
-	_ = Executer(pf[:])
+	pf := getFuncs(100)
+	_ = executer.startTasks(pf[:], 2)
 
 }
 
-func getFuncs(n int) []func() error {
-
-	var result []func() error
+func getFuncs(n int) []Task {
+	runtime.GOMAXPROCS(4)
+	var result []Task
 	for i := 0; i < n; i++ {
 		z := i
-
+		timeExec := time.Millisecond * time.Duration(rand.Intn(1000))
 		f := func() error {
-			fmt.Println("Message from func #", z)
-
+			time.Sleep(timeExec)
+			fmt.Printf("Message from func #%v, time exection %v \n", z, timeExec)
 			if rand.Intn(100) > 80 {
 				return errors.New("error in function")
 			} else {
